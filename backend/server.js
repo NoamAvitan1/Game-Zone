3b8c8d908a5e754a1ad0635d1f8ea8caaed87f13
Noam Avitan <noamavitan159753@gmail.com>
2023-08-09 21:05:59 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://jocular-mermaid-934913.netlify.app',
   credentials: true
 }));
 app.use(cookieParser());
