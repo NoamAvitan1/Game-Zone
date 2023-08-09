7687ad7ae76e819ffa8b54b1dd542b414883d867
Noam Avitan <noamavitan159753@gmail.com>
2023-08-09 20:41:04 +0300

    socket remove

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const http = require('http');
-const { Server } = require('socket.io');
+
 
 //routes
 const userRoutes = require('./routes/user');
@@ -48,24 +48,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // Create an HTTP server
     const server = http.createServer(app);
-
-    // Initialize socket.io
-    const io = new Server(server, {
-      cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
-      }
-    });
-
-    // Handle socket.io events
-    io.on('connection', (socket) => {
-      console.log(`A user connected :${socket.id}`);
-
-      socket.on("send_Message", (data) => {
-        socket.broadcast.emit("received_message", data);
-      });
-    });
-
     // Start the server
     server.listen(process.env.PORT, () => {
       console.log('Listening on port', process.env.PORT);
