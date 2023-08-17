17e4ceb0f7a89aa9db24e49a5efcf9890526db52
Noam Avitan <noamavitan159753@gmail.com>
2023-08-17 13:31:49 +0300

    update

--- frontend/src/components/games/2048/Game2048.jsx
+++ frontend/src/components/games/2048/Game2048.jsx
@@ -1,31 +1,39 @@
 import React, { useState, useEffect } from "react";
 import "./Game2048.css";
-import EndedGameAllert from '../../reusfullComponents/endedGameAllert/endedGameAllert';
-import useUser from '../../../hooks/useUser';
+import EndedGameAllert from "../../reusfullComponents/endedGameAllert/endedGameAllert";
+import useUser from "../../../hooks/useUser";
 export const Game2048 = () => {
   const [board, setBoard] = useState(
     Array(4)
       .fill(null)
       .map(() => Array(4).fill(null))
   );
-  const [score,setScore] = useState(0);
-  const [winner,setWinner] = useState(false);
-  const [gameDone , setGameDone ] = useState(null);
-  const {user , updateXp} = useUser();
+  const [score, setScore] = useState(0);
+  const [winner, setWinner] = useState(false);
+  const [failed, setFailed] = useState(false);
+  const [gameDone, setGameDone] = useState(null);
+  const { user, updateXp } = useUser();
 
-   const points = (p) => {
-    setScore(prevScore => prevScore + p);
-   };
-
-   const reset = () => {
-        for (let i = 0; i < board.length; i++){
-          for (let j = 0; j < board[i].length; j++){
-            board[i][j] = null;
-          }
+  const points = () => {
+    let max = score;
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] > max) {
+          setScore(board[i][j]);
         }
-        setScore(0);
-        fillCells();
-   }
+      }
+    }
+  };
+
+  const reset = () => {
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        board[i][j] = null;
+      }
+    }
+    setScore(0);
+    fillCells();
+  };
 
   const rndCell = (cell) => {
     const nullz = [];
@@ -43,25 +51,25 @@ export const Game2048 = () => {
   const fillCells = (empty = true) => {
     const a = rndCell();
     let b = null;
-    if(empty) b = rndCell(a);
+    if (empty) b = rndCell(a);
     const newBoard = [...board];
     if (a) newBoard[a.row][a.col] = 2;
     if (empty && b) newBoard[b.row][b.col] = 2;
     setBoard(newBoard);
+    if (a || b) {
+      return true;
+    }
+    return false;
   };
 
-
   const checkWinner = () => {
-    let failed = true;
     for (let i = 0; i < 4; i++) {
-      if(board[i].includes(2048))
-        return {winner , failed:false};
-      if(failed && board[i].includes(null))
-        failed = false;
+      if (board[i].includes(2048)) {
+        return true;
+      }
+      return false;
     }
-    return {winner:false , failed};
-}
-  
+  };
 
   // const checkEmpty = () => {
   //   for (let i = 0; i < 4; i++) if (board[i].includes(2)) return false;
@@ -83,7 +91,6 @@ export const Game2048 = () => {
           }
           if (arr[k + 1 + index] == arr[k + index] && arr[k + index] != null) {
             arr[k + 1 + index] = arr[k + index] + arr[k + index];
-            points(arr[k + index] + arr[k + index])
             arr[k + index] = null;
           }
           index++;
@@ -92,105 +99,114 @@ export const Game2048 = () => {
       newArr.push(arr);
     }
     setBoard(newArr);
-    fillCells(false)
+    let flag = fillCells(false);
+    return flag;
   };
-
 
   const moveLeft = () => {
     let newArr = [];
     for (let d = 0; d < 4; d++) {
-    let arr = board[d];
-    let i = 0;
-    let index;
-    for (i = 0; i < 3; i++) {
-      index = 0;
-      for (let j = 0, k = i + 1; j < 1 + i; j++) {
-        index = Math.abs(index);
-        if (arr[k - 1 - index] == null && arr[k - index] != null) {
-          arr[k - 1 - index] = arr[k - index];
-          arr[k - index] = null;
+      let arr = board[d];
+      let i = 0;
+      let index;
+      for (i = 0; i < 3; i++) {
+        index = 0;
+        for (let j = 0, k = i + 1; j < 1 + i; j++) {
+          index = Math.abs(index);
+          if (arr[k - 1 - index] == null && arr[k - index] != null) {
+            arr[k - 1 - index] = arr[k - index];
+            arr[k - index] = null;
+          }
+          if (arr[k - 1 - index] == arr[k - index] && arr[k - index] != null) {
+            arr[k - 1 - index] = arr[k - index] + arr[k - index];
+            arr[k - index] = null;
+          }
+          index *= -1;
+          index--;
         }
-        if (arr[k - 1 - index] == arr[k - index] && arr[k - index] != null) {
-          arr[k - 1 - index] = arr[k - index] + arr[k - index];
-          points(arr[k - index] + arr[k - index])
-          arr[k - index] = null;
-        }
-        index *= -1;
-        index--;
       }
+      newArr.push(arr);
     }
-    newArr.push(arr);
-  }
-  setBoard(newArr);
-  fillCells(false);
+    setBoard(newArr);
+    let flag = fillCells(false);
+    return flag;
   };
 
   const moveDown = () => {
-        let arr = [...board];
-        let index;
-        for(let i=0; i<4;i++){
-            for(let d =0; d<3;d++){
-                index = 0;
-                for(let j=0,k=2-d;j<d+1;j++){
-                    if(arr[k+1+index][i] == null && arr[k+index][i]!= null){
-                        arr[k+1+index][i] = arr[k+index][i];
-                        arr[k+index][i] = null;
-                    }
-                    if(arr[k+1+index][i] == arr[k+index][i] && arr[k+index][i]!= null){
-                        arr[k+1+index][i] = arr[k+index][i] + arr[k+index][i];
-                        points(arr[k+index][i] + arr[k+index][i] )
-                        arr[k+index][i] = null;
-                    }
-                    index++;
-                }
-            }
-  }
-  setBoard(arr);  
-  fillCells(false);  
-}
+    let arr = [...board];
+    let index;
+    for (let i = 0; i < 4; i++) {
+      for (let d = 0; d < 3; d++) {
+        index = 0;
+        for (let j = 0, k = 2 - d; j < d + 1; j++) {
+          if (arr[k + 1 + index][i] == null && arr[k + index][i] != null) {
+            arr[k + 1 + index][i] = arr[k + index][i];
+            arr[k + index][i] = null;
+          }
+          if (
+            arr[k + 1 + index][i] == arr[k + index][i] &&
+            arr[k + index][i] != null
+          ) {
+            arr[k + 1 + index][i] = arr[k + index][i] + arr[k + index][i];
+            arr[k + index][i] = null;
+          }
+          index++;
+        }
+      }
+    }
+    setBoard(arr);
+    let flag = fillCells(false);
+    return flag;
+  };
 
   const moveUp = () => {
     let arr = [...board];
     let index;
-    for(let i=0; i<4;i++){
-      for(let d =0; d<3;d++){
-          index = 0;
-         for(let j=0,k=1+d;j<d+1;j++){
-              index = Math.abs(index);
-              if(arr[k-1-index][i] == null && arr[k-index][i]!= null){
-                  arr[k-1-index][i] = arr[k-index][i];
-                  arr[k-index][i] = null;
-              }
-              if(arr[k-1-index][i] == arr[k-index][i] && arr[k-index][i]!= null){
-                  arr[k-1-index][i] = arr[k-index][i] + arr[k-index][i];
-                  points(arr[k-index][i] + arr[k-index][i])
-                  arr[k-index][i] = null;
-              }
-              index *= -1;
-              index--;
+    for (let i = 0; i < 4; i++) {
+      for (let d = 0; d < 3; d++) {
+        index = 0;
+        for (let j = 0, k = 1 + d; j < d + 1; j++) {
+          index = Math.abs(index);
+          if (arr[k - 1 - index][i] == null && arr[k - index][i] != null) {
+            arr[k - 1 - index][i] = arr[k - index][i];
+            arr[k - index][i] = null;
           }
+          if (
+            arr[k - 1 - index][i] == arr[k - index][i] &&
+            arr[k - index][i] != null
+          ) {
+            arr[k - 1 - index][i] = arr[k - index][i] + arr[k - index][i];
+            arr[k - index][i] = null;
+          }
+          index *= -1;
+          index--;
+        }
       }
-  }
-  setBoard(arr);  
-  fillCells(false);  
+    }
+    setBoard(arr);
+    let flag = fillCells(false);
+    return flag;
   };
 
-
-  const handleKeyPress = (event) => { 
-    // if(winner){
-    //   return;
-    // }
+  const handleKeyPress = (event) => {
+    let right, down, up, left;
+    if (winner) {
+      return;
+    }
     if (event.key === "ArrowRight") {
-      moveRight();
+      right = moveRight();
     }
     if (event.key === "ArrowDown") {
-      moveDown();
+      down = moveDown();
     }
     if (event.key === "ArrowLeft") {
-      moveLeft();
+      left = moveLeft();
     }
     if (event.key === "ArrowUp") {
-      moveUp();
+      up = moveUp();
+    }
+    if (right && down && left && up) {
+      setFailed(true);
     }
   };
 
@@ -204,41 +220,43 @@ export const Game2048 = () => {
     };
   }, []);
 
-
   useEffect(() => {
     fillCells();
   }, []);
 
-
   useEffect(() => {
-    
-    let {winner , failed} = checkWinner();
-    console.log({winner , failed});
-    if (winner) {
-      if(user)
-        updateXp(score);
+    if (checkWinner()) {
+      if (user) updateXp(score);
       setGameDone(
-        <EndedGameAllert 
-          message={"Good Game"} 
-          xp={score} 
-          restart={()=>{
+        <EndedGameAllert
+          message={"Good Game"}
+          xp={score}
+          restart={() => {
             reset();
             setGameDone(null);
-          }}/>)
+          }}
+        />
+      );
       setWinner(true);
     }
-      
+
     if (failed)
       setGameDone(
-      <EndedGameAllert 
-        message={"Good Game"} 
-        xp={score} 
-        restart={()=>{
-          reset();
-          setGameDone(null);
-        }}/>)
-  },[score])
+        <EndedGameAllert
+          message={"Good Game"}
+          xp={score}
+          restart={() => {
+            reset();
+            setGameDone(null);
+          }}
+        />
+      );
+  }, [score]);
 
+
+  useEffect(() => {
+      points();
+  },[board])
 
   return (
     <div className="Game">
@@ -250,15 +268,25 @@ export const Game2048 = () => {
       <section className="grid">
         {board.map((r) =>
           r.map((c, i) => (
-            <div key={i} className="div" style={{backgroundColor:(c ? `hsl(${(c * 10)%360},50%,50%)` : 'var(--light-bg)')}}>
+            <div
+              key={i}
+              className="div"
+              style={{
+                backgroundColor: c
+                  ? `hsl(${(c * 10) % 360},50%,50%)`
+                  : "var(--light-bg)",
+              }}
+            >
               {c}
             </div>
           ))
         )}
       </section>
-       <div>
-          <button onClick={()=>reset()} className="reset">Reset</button>
-        </div>
+      <div>
+        <button onClick={() => reset()} className="reset">
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
